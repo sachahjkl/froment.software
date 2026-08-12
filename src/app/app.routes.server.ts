eb587d0c7433a723f00c31135abef78bb8d920f8
sachahjkl <sacha@sacha.house>
2026-08-12 16:58:29 +0000

    Serve protected client routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'back-office',
+    renderMode: RenderMode.Prerender,
+  },
+  {
     path: 'back-office/**',
     renderMode: RenderMode.Client,
   },
